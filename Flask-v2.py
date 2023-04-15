b72c72a721d4e43f0f62fe4e9ac3d28986fa21b5
Prateek <110811408+Prateikx@users.noreply.github.com>
2023-04-15 22:54:22 +0530

    Update Flask-v2.py

--- Flask-v2.py
+++ Flask-v2.py
@@ -1,25 +1,27 @@
+# flask-v1.py is the most updated Version
+
 from flask import Flask, request, jsonify, render_template
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
-import json
 import mariadb
 import pickle
 
-le = LabelEncoder()
-app = Flask(__name__) # template_folder='E:\Bignalytics\my_project\templates')
 
+le = LabelEncoder()
+app = Flask(__name__)
+
+# Load the trained model
+with open('model_pickle', 'rb') as f:
+    model = pickle.load(f)
+
+# Load database configuration from environment variables
 config = {
-    'host': '127.0.0.1',
+    'host': 'localhost',
     'port': 3306,
     'user': 'root',
     'password': 'admin',
     'database': 'airbnb_new'
 }
-
-
-# Load the trained model
-with open('E:/Bignalytics/my_project/model_pickle', 'rb') as f:
-    model = pickle.load(f)
 
 @app.route('/')
 def index():
@@ -27,76 +29,59 @@ def index():
 
 @app.route('/data')
 def data():
-    conn = mariadb.connect(**config)
-    cur = conn.cursor()
-    sql = "SELECT * FROM new_data;"
-    cur.execute(sql)
-    row_headers=[x[0] for x in cur.description]
-    rv = cur.fetchall()
-    json_data=[]
-    for result in rv:
-            json_data.append(dict(zip(row_headers,result)))
-    return json.dumps(json_data)
-
+    try:
+        conn = mariadb.connect(**config)
+        cur = conn.cursor()
+        sql = "SELECT * FROM new_data;"
+        cur.execute(sql)
+        data = cur.fetchall()
+        conn.close()
+        return render_template('data.html', data=data)
+    except mariadb.Error as e:
+        return jsonify(error=str(e)), 500
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get the input data from the form
-    room_type = request.form['room_type']
-    accommodates = int(request.form['accommodates'])
-    bathrooms = int(request.form['bathrooms'])
-    cancellation_policy = request.form['cancellation_policy']
-    cleaning_fee = request.form['cleaning_fee']
-    instant_bookable = request.form['instant_bookable']
-    bedrooms = int(request.form['bedrooms'])
-    beds = int(request.form['beds'])
+    data = {
+        'room_type': request.form['room_type'],
+        'accommodates': int(request.form['accommodates']),
+        'bathrooms': int(request.form['bathrooms']),
+        'cancellation_policy': request.form['cancellation_policy'],
+        'cleaning_fee': request.form['cleaning_fee'],
+        'instant_bookable': request.form['instant_bookable'],
+        'bedrooms': int(request.form['bedrooms']),
+        'beds': int(request.form['beds'])
+    }
 
-    conn = mariadb.connect(**config)
-    # create a connection cursor
-    cur = conn.cursor()
-    # execute a SQL statement
-    sql = "insert into new_data (room_type, accommodates, bathrooms, cancellation_policy, cleaning_fee, instant_bookable, bedrooms, beds) VALUES (%s, %s, %s,%s, %s, %s,%s, %s)"
-    vals = (room_type, accommodates, bathrooms, cancellation_policy, cleaning_fee , instant_bookable ,bedrooms ,beds)
-    cur.execute(sql, vals)
+    try:
+        conn = mariadb.connect(**config)
+        cur = conn.cursor()
+        sql = "INSERT INTO new_data (room_type, accommodates, bathrooms, cancellation_policy, cleaning_fee, instant_bookable, bedrooms, beds) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+        vals = tuple(data.values())
+        cur.execute(sql, vals)
+        conn.commit()
 
-    # execute a SELECT query and get the results as a DataFrame
-    select = 'SELECT * FROM new_data ORDER BY id DESC LIMIT 2'
-    df_sql = pd.read_sql(select, conn)
-    
-    conn.commit()
-    # Close the database connection
-    cur.close()
-    conn.close()
+        cur.close()
+        conn.close()
+    except mariadb.Error as e:
+        return jsonify(error=str(e)), 500
 
-    # print( df_sql) # It will print last 2 rows from the MariaDB database
-
-
-    # Store the values in a dictionary
-    data = {'room_type': room_type,
-            'accommodates': accommodates,
-            'bathrooms': bathrooms,
-            'cancellation_policy': cancellation_policy,
-            'cleaning_fee': cleaning_fee,
-            'instant_bookable': instant_bookable,
-            'bedrooms': bedrooms,
-            'beds': beds}
-
-    # Create a DataFrame from the dictionary
     df = pd.DataFrame.from_dict([data])
-    
-    cat_cols = ['room_type','cancellation_policy','cleaning_fee','instant_bookable']
+    odf = df.copy()
 
-    for col in cat_cols:
-        df[col] = le.fit_transform(df[col])
-        odf = pd.concat([df, pd.get_dummies(df[col], prefix=col, drop_first=False)], axis=1)
-        odf.drop(col, axis=1, inplace=True)
-    
+#Performing Label Encoding before passing into the model
+    odf['room_type'] = le.fit_transform(odf['room_type'])
+    odf['cancellation_policy'] = le.fit_transform(odf['cancellation_policy'])
+    odf['cleaning_fee'] = le.fit_transform(odf['cleaning_fee'])
+    odf['instant_bookable'] = le.fit_transform(odf['instant_bookable'])
 
     pred = model.predict(odf)
+    pred = round(pred[0], 3)
     print('Prediction: ',pred)
     
     # Render the result page with the DataFrame as a HTML table
-    return render_template('result.html', data=df.to_html(index=False), res=pred[0])
+    return render_template('result.html', data=df.to_html(index=False), res=pred)
 
 
 @app.errorhandler(400)
@@ -104,4 +89,4 @@ def bad_request(e):
     return jsonify(error=str(e)), 400
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8505)
+    app.run(debug=True, port=8055)
